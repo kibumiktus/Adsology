e8f42e348d7bab170579b12354b0da1ef365b9c0
kibumiktus <myhajlo.prozorov@gmail.com>
2020-06-24 12:32:09 +0300

    Add loading from DB

---
+++ Adsology.Dal/Models/OrderStatuses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Adsology.Dal.Models
+{
+    public partial class OrderStatuses
+    {
+        public OrderStatuses()
+        {
+            Orders = new HashSet<Orders>();
+        }
+
+        public byte Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
