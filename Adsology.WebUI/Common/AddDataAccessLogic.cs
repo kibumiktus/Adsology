6a03fdf1fbbe1200ca660b2745682e6ba1805405
kibumiktus <myhajlo.prozorov@gmail.com>
2020-07-01 01:14:26 +0300

    Add UI

---
+++ Adsology.WebUI/Common/AddDataAccessLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Adsology.Dal;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Adsology.WebUI.Common
+{
+    public static class DataAccessLogicExtension
+    {
+        public static IServiceCollection AddDataAccessLogic(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AdsologyDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("AdsologyDatabase")));
+
+            services.AddScoped<IAdsologyDbContext>(provider => provider.GetService<AdsologyDbContext>());
+
+            return services;
+        }
+    }
+}
---
+++ Adsology.WebUI/Controllers/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Adsology.BusinessLogic;
+using Adsology.BusinessLogic.Model.OrdersXmlImportModel;
+using Adsology.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adsology.WebUI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController: ControllerBase
+    {
+        private readonly OrdersService _ordersService;
+
+        public OrdersController(OrdersService ordersService)
+        {
+            _ordersService = ordersService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Import(HttpRequestMessage request)
+        {
+            var contentStream = await request.Content.ReadAsStreamAsync();
+            await _ordersService.ImportFromXml(contentStream);
+            return Ok();
+        }
+    }
+}
